3deab16ef225a47a2784fa647dc170d0ac47714c
ariyangd <57576103+ariyangd@users.noreply.github.com>
2021-01-22 05:48:43 +0800

    first commit

---
+++ assignment.js
@@ -0,0 +1,10 @@
+
+//Kilo Meter to Meter 
+function kilometerToMeter(km){
+    var meter = km * 1000;
+    return meter;
+}
+var convert = kilometerToMeter(10);
+console.log('Kilo Meter to Meter is: ',convert);
+
+//Kilo Meter to Meter 
